60bee897e93a6d6240799af5ff8473b6999cf4e0
Dawid <dawid.stuczyk@wp.pl>
2021-10-23 14:18:55 +0200

    Dodaj pliki projektów.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Forum_dyskusyjne.Startup))]
+namespace Forum_dyskusyjne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
