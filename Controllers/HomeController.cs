45410b8971310e55aeba09cbcbeb728ff9aaeeaa
Dawid <dawid.stuczyk@wp.pl>
2021-11-18 00:41:51 +0100

    Uber basic home index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Forum_dyskusyjne.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,10 @@ namespace Forum_dyskusyjne.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Categories.ToList());
         }
 
         public ActionResult About()
